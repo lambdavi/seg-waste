281ebd59e8e19e414420162a915c4d9e736daff3
Davide Buoso <daviibuoso@gmail.com>
2023-07-03 23:28:25 +0200

    test with 2 classes

--- train.py
+++ train.py
@@ -128,7 +128,7 @@ def validate(val_loader, net, criterion, optimizer, epoch, restore):
             outputs[outputs<=0.5] = 0
             #print(outputs)
             #print(labels)
-            update_metric(val_metric, outputs, labels)
+            val_metric.update(labels, outputs)
         
         iou_ += calculate_mean_iu([outputs.squeeze_(1).data.cpu().numpy()], [labels.data.cpu().numpy()], 2)
     mean_iu = iou_/len(val_loader)   
